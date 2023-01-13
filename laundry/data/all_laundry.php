ee0b77f480558f41c684b5930912225972db40c6
Ihya Ulumuddin <ihya14algazali@gmail.com>
2023-01-13 08:41:26 +0800

    Revisi

---
+++ laundry/data/all_laundry.php
@@ -0,0 +1,54 @@
+<br />
+<?php 
+require_once('../class/Laundry.php');
+$laundries = $laundry->all_laundry();
+ ?>
+
+<div class="table-responsive">
+        <table id="myTable-laundry" class="table table-bordered table-hover table-striped">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Nama </th>
+                    <th><center>Urutan #</center></th>
+                    <th><center>Berat</center></th>
+                    <th><center>Tipe</center></th>
+                    <th><center>Tanggal</center></th>
+                    <th><center>Total</center></th>
+                    <th><center>Opsi</center></th>
+                </tr>
+            </thead>
+            <tbody>
+            	<?php
+                    foreach($laundries as $l): 
+                    $amount = $l['laun_weight'] * $l['laun_type_price'];
+                ?>
+                <tr align="center">
+                    <td><input type="checkbox" name="imSlepy" value="<?= $l['laun_id']; ?>"></td>
+                    <td align="left"><?= ucwords($l['customer_name']); ?></td>
+                    <td><?= $l['laun_priority']; ?></td>
+                    <td><?= $l['laun_weight']; ?></td>
+                    <td><?= $l['laun_type_desc']; ?></td>
+                    <td><?= $l['laun_date_received']; ?></td>
+                    <td><?= '₱ '.number_format($amount, 2); ?></td>
+                    <td>
+                        <button onclick="editLaundry('<?= $l['laun_id']; ?>')" type="button" class="btn btn-warning btn-xs">
+                           Edit
+                           <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+                        </button>
+                    </td>
+                </tr>
+	            <?php endforeach; ?>
+            </tbody>
+        </table>
+</div>
+
+
+<!-- for the datatable of employee -->
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#myTable-laundry').DataTable();
+    });
+</script>
+
+<?php $laundry->Disconnect(); ?>
---
+++ laundry/data/insert_laundry.php
@@ -0,0 +1,21 @@
+<?php 
+require_once('../class/Laundry.php');
+if(isset($_POST['customer'])){
+	$customer = $_POST['customer'];
+	$priority = $_POST['priority'];
+	$weight = $_POST['weight'];
+	$type = $_POST['type'];
+
+	$customer = strtolower($customer);
+	$customer = ucwords($customer);
+	var_dump($_POST);
+
+	$saveLaundry = $laundry->new_laundry($customer, $priority, $weight, $type);
+	$return['valid'] = false;
+	if($saveLaundry){
+		$return['valid'] = true;
+		$return['msg'] = 'New Laundry Added Successfully!';
+	}
+	echo json_encode($return);
+}//end isset
+$laundry->Disconnect();
